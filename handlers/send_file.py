8d2b24a29ba67d87bcfa89f440506f142fba0dd7
searchingshiv <108224889+searchingshiv@users.noreply.github.com>
2023-12-24 16:25:47 +0530

    Update send_file.py

--- handlers/send_file.py
+++ handlers/send_file.py
@@ -8,16 +8,6 @@ from pyrogram.types import Message
 from pyrogram.errors import FloodWait
 from handlers.helpers import str_to_b64
 
-async def reply_forward(message: Message, file_id: int):
-    try:
-        await message.reply_text(
-            f"Files will be deleted in 30 minutes to avoid copyright issues. Please forward and save them.",
-            disable_web_page_preview=True,
-            quote=True
-        )
-    except FloodWait as e:
-        await asyncio.sleep(e.x)
-        await reply_forward(message, file_id)
 
 async def media_forward(bot: Client, user_id: int, file_id: int):
     try:
@@ -34,7 +24,6 @@ async def media_forward(bot: Client, user_id: int, file_id: int):
 
 async def send_media_and_reply(bot: Client, user_id: int, file_id: int):
     sent_message = await media_forward(bot, user_id, file_id)
-    await reply_forward(message=sent_message, file_id=file_id)
     asyncio.create_task(delete_after_delay(sent_message, 1800))
 
 async def delete_after_delay(message, delay):
